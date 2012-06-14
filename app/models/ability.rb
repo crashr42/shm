1d7bb81b8bcf7bf4e0e96a260939d5e43cd706d0
nikita <nikita.kem@gmail.com>
2012-06-14 20:07:48 +0700

    #10 Только админы могут зайти в админку.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,5 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new
+    if user.role? :admin
+      can :access, :rails_admin
+      can :dashboard
+      can :manage, :all
+    end
   end
 end
