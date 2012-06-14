90fc4f915c93d9d913746a91d35eb1aa0d5abaea
nikita <nikita.kem@gmail.com>
2012-06-14 15:38:56 +0700

    Вставка тестовых данных.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+User.delete_all
+
+Rake::Task['users:add_test'].invoke
+
+(0..50).each do
+  u = User.new
+  u.email = Faker::Internet.email
+  u.password = Faker::Lorem.characters 15
+  u.save!
+end
+
