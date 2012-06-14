4b9f0cece5250bd4138a120fb16b4da314bdb7d9
nikita <nikita.kem@gmail.com>
2012-06-14 19:38:42 +0700

    #10 Добавлен метод на проверку наличия у пользователя роли.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   attr_protected :created_at, :current_sign_in_at, :current_sign_in_ip, :encrypted_password,
                  :id, :last_sign_in_at, :last_sign_in_ip, :remember_created_at,
                  :reset_password_sent_at, :reset_password_token, :sign_in_count, :updated_at, :users_to_roleses
+
+  def role?(name)
+    self.roles.find_by_name(name)
+  end
 end
