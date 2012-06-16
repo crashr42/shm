d5c2f507284210c32ef47ecc779b780e210bcc26
nikita <nikita.kem@gmail.com>
2012-06-16 16:17:33 +0700

    #5 Переименовано поле.

--- db/migrate/20120616055350_create_events.rb
+++ db/migrate/20120616055350_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.references :calendar
 
-      t.datetime  :dtstart,     :null => false, :default => DateTime.now
+      t.datetime  :date_start,     :null => false, :default => DateTime.now
       t.string    :description
       t.string    :status,      :null => false, :default => 'CONFIRMED'
       t.string    :summary,     :null => false
