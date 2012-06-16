d8ac0c2e8d175d457de1eb040973d7a5b44f2637
nikita <nikita.kem@gmail.com>
2012-06-16 12:54:39 +0700

    #5 Добавлена модель календаря.

---
+++ db/migrate/20120616053421_create_calendars.rb
@@ -0,0 +1,14 @@
+class CreateCalendars < ActiveRecord::Migration
+  def up
+    create_table :calendars do |t|
+      t.string  :name,        :null => false
+      t.text    :description
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :calendars
+  end
+end
