c04696d7e6e065156d963e19837c13d3c25f4a06
nikita <nikita.kem@gmail.com>
2012-06-16 18:20:27 +0700

    #5 Добавлены методы для доступа к свойствам.

--- app/models/recurrence_rule.rb
+++ app/models/recurrence_rule.rb
@@ -1,3 +1,27 @@
 class RecurrenceRule < ActiveRecord::Base
   belongs_to :event
+
+  %w{
+    seconds minutes hours week_days
+    month_days year_days weeks months position
+  }.each do |p|
+    define_method "add_#{p.chomp('s')}" do |value|
+      eval """
+        self.#{p} = '' unless self.#{p}
+        unless self.#{p}.split(',').include?(#{value}.to_s)
+          self.#{p} = self.#{p}.split(',').push(#{value}).join(',')
+        end
+      """
+    end
+
+    define_method "remove_#{p.chomp('s')}" do |value|
+      eval """
+        self.#{p} = '' unless self.#{p}
+        s = self.#{p}.split(',')
+        s.delete(#{value}.to_s)
+        self.#{p} = s == '' ? nil : s.join(',')
+      """
+    end
+  end
+
 end
