9d0c34d81a2cea452b962134c518e8e3b0ae0e53
nikita <nikita.kem@gmail.com>
2012-06-19 17:31:49 +0700

    #11 Текущий пользователь сохраняется в модели.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :manage_user
+
+  private
+  def manage_user
+    User.current = current_user
+  end
+
   if ::Rails.application.config.handle_errors
     rescue_from Exception, :with => :render_error
     rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
     rescue_from CanCan::AccessDenied, :with => :render_access_denied
   end
 
-  private
   def render_error(exception)
     redirect_to "/error/500"
   end
