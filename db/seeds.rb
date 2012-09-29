92aba36edbd40eb6a82697112b7b6316583398c4
nikita <nikita.kem@gmail.com>
2012-09-29 12:18:40 +0700

    #24 update db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@ Role.create :name => 'doctor'
 Role.create :name => 'admin'
 Role.create :name => 'manager'
 
+RecurrenceRule.delete_all
+Event.delete_all
+Calendar.delete_all
 User.delete_all
 
 Rake::Task['users:add_test'].invoke
@@ -21,7 +24,7 @@ Role.all.each do |role|
   ur = UsersToRoles.new
   ur.user = user
   ur.role = role
-  ur.save
+  ur.save!
 end
 
 (0..50).each do
@@ -33,5 +36,38 @@ end
   ur.user = u
   ur.role = Role.find_by_name 'patient'
   ur.save!
+
+  c = Calendar.new
+  c.name = "Calendar for #{u.email}"
+  (0..30).each do |i|
+    e = Event.new
+    e.calendar = c
+    e.summary = "Summary for event #{i} by user #{u.email}"
+    e.description = "Description for event #{i} by user #{u.email}"
+    e.date_start = DateTime.now - Random.rand(1..3600).minutes
+    e.time_start = e.date_start
+    e.date_end = DateTime.now + Random.rand(1..3600).minutes
+
+    (0..Random.rand(0..2)).each do |ir|
+      r = RecurrenceRule.new
+      r.count = Random.rand(0..10)
+      r.end_date = DateTime.now + Random.rand(1..7200).minutes
+      r.frequency = :yearly
+      r.hours = Random.rand(0..24)
+      r.interval = 1
+      r.minutes = Random.rand(0..60)
+      r.month_days = Random.rand(0..31)
+      r.months = Random.rand(0..12)
+      r.seconds = Random.rand(0..60)
+      r.event = e
+      r.save!
+    end
+    e.user = u
+    e.save!
+  end
+  c.user = u
+  c.save!
 end
+
+
 
