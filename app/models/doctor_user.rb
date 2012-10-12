71fbab0be51632b5e18acbb9ff1fc1fde85ac4c4
nikita <nikita.kem@gmail.com>
2012-10-12 20:24:01 +0700

    #18 fix query for select appointments hours for doctor

--- app/models/doctor_user.rb
+++ app/models/doctor_user.rb
@@ -4,6 +4,6 @@ class DoctorUser < User
   default_scope :readonly => false
 
   def appointments_hours
-    Event.joins(:attendees).where(:attendees => {:user_id => id})
+    Event.joins(:attendees).where(:category => 'appointment_hour').where(:attendees => {:user_id => id, :role => 'attending_doctor'})
   end
 end
