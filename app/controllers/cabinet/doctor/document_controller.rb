f0ac87b4b2e7449bd7eb638e700d26c32ce03755
DenisKem <denis.kem@gmail.com>
2012-10-22 04:08:05 +0700

    #33 Adding a creating document by doctor

--- app/controllers/cabinet/doctor/document_controller.rb
+++ app/controllers/cabinet/doctor/document_controller.rb
@@ -1,5 +1,16 @@
 class Cabinet::Doctor::DocumentController < ApplicationController
   def new
-    @document = Document.new
+    begin
+      @document = Document.new
+      @document.event_id = @_params[:event_id]
+      @document.record = @_params[:doc_content]
+      @document.user = User.current
+      @document.save
+      flash[:notice] = 'Document was succefly cretating'
+    rescue
+      flash[:error] = "Errror - can't create document"
+    end
+
+    redirect_to :controller => "index", :action => :index
   end
 end
