5248ac03d6dc7c67953c55c412b865eedc1b60a4
Denis <denis.kem@gmail.com>
2012-11-04 21:21:35 +0700

    #36 add create event with type appointment

--- app/controllers/cabinet/doctor/appointment_controller.rb
+++ app/controllers/cabinet/doctor/appointment_controller.rb
@@ -1,5 +1,7 @@
 class Cabinet::Doctor::AppointmentController < ApplicationController
   
+  layout 'cabinet/doctor/layout'
+  
   #get appointment index page (earh and select patient)
   def index 
 
@@ -12,6 +14,28 @@ class Cabinet::Doctor::AppointmentController < ApplicationController
 
   #create new appontment
   def create
+   @event = Event.new params[:event]
+
+   respond_to do |f|
+     #if @event.save
+      #  f.html { redirect_to({:action => :show, :id => @event.id}, :notice => 'Event created.') }
+     #else
+      #  f.html { render :action => :new }
+     #end
+     begin
+       @event.save
+       f.html { redirect_to({:action => :show, :id => @event.id}, :notice => 'Event created.')}
+     rescue Exception => exp
+        puts exp.message
+        flash[:error] = exp.message
+        f.html { render :action => :new }
+     end
+
+   end
+  end
+
+  #show appointment
+  def show
    
   end
 end
