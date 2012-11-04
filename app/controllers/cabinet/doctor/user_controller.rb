5fe2e5b32e8dadf29a43c3b762dd4b301f152050
Denis <denis.kem@gmail.com>
2012-11-04 18:00:25 +0700

    #36 Add  case-insensitive searh

--- app/controllers/cabinet/doctor/user_controller.rb
+++ app/controllers/cabinet/doctor/user_controller.rb
@@ -3,7 +3,7 @@ class Cabinet::Doctor::UserController < ApplicationController
   #find user by name
   def find
     @users = User.select([:id, :first_name, :last_name, :email]).where(
-    'first_name like :name or last_name like :name', {:name => "%#{params[:name]}%"}).where('type = ?', "PatientUser").limit(200)
+    'first_name ILIKE :name or last_name ILIKE :name', {:name => "%#{params[:name]}%"}).where('type = ?', "PatientUser").limit(200)
 
     respond_to do |f|
       f.html { render :layout => false }
