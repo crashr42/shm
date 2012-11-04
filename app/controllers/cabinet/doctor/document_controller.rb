e2da0f40475e5f6895355641b651a072681527e0
Denis <denis.kem@gmail.com>
2012-11-04 09:27:30 +0700

    #33 Fix document controller. Add notice

--- app/controllers/cabinet/doctor/document_controller.rb
+++ app/controllers/cabinet/doctor/document_controller.rb
@@ -18,17 +18,29 @@ class Cabinet::Doctor::DocumentController < ApplicationController
   
   #Creting new document
   def create 
+    if params[:event_id].blank?
+      flash[:error] = "No event_id in params"
+      #return redirect_to '/cabinet/doctor/document/index'
+    end
+
+    if params[:doc_content].blank?
+      flash[:error] = "You must all field complete"
+      #return redirect_to '/cabinet/doctor/document/index'
+    end
+
+
     begin
       @document = Document.new
-      @document.event_id = @_params[:event_id]
-      @document.record = @_params[:doc_content]
+      @document.event_id = params[:event_id]
+      @document.record = params[:doc_content]
       @document.user = User.current
       @document.save
       flash[:notice] = 'Document was succefly cretating'
+      #return redirect_to '/cabinet/doctor/document/index'
     rescue
       flash[:error] = "Errror - can't create document"
     end
 
-    redirect_to '/cabinet/doctor/document/index'
+    return redirect_to '/cabinet/doctor/document/index'
   end
 end
