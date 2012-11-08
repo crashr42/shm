c5a330c334d926ab01741b4c9b397cb6b10c5fe2
Denis <denis.kem@gmail.com>
2012-11-08 12:05:51 +0700

    Close #36. There are a new appointment making by doctor in himself cabinet

--- app/controllers/cabinet/doctor/appointment_controller.rb
+++ app/controllers/cabinet/doctor/appointment_controller.rb
@@ -19,13 +19,21 @@ class Cabinet::Doctor::AppointmentController < ApplicationController
    @event = Event.new params[:event]
 
    respond_to do |f|
-     #if @event.save
-      #  f.html { redirect_to({:action => :show, :id => @event.id}, :notice => 'Event created.') }
-     #else
-      #  f.html { render :action => :new }
-     #end
-     begin
+       begin
        @event.save
+
+       @at_doctor = Attendee.new
+       @at_doctor.user_id = params['doctor_id']
+       @at_doctor.event_id = @event.id
+       @at_doctor.role = 'doctor'
+       @at_doctor.save
+
+       @at_patient = Attendee.new
+       @at_patient.user_id = params['patient_id']
+       @at_patient.event_id = @event.id
+       @at_patient.role = 'patient'
+       @at_patient.save
+
        f.html { redirect_to({:action => :show, :id => @event.id}, :notice => 'Event created.')}
      rescue Exception => exp
         puts exp.message
