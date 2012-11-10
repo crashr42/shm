d223af97160d9594f74a4378d474c0646823044b
nikita <nikita.kem@gmail.com>
2012-11-10 18:09:38 +0700

    #70 add comments

--- app/models/attendee/attending_doctor_attendee.rb
+++ app/models/attendee/attending_doctor_attendee.rb
@@ -2,6 +2,8 @@ class AttendingDoctorAttendee < Attendee
   after_create :add_attending_doctor_to_child
 
   private
+  # При создании участника доктора, если событие к которому привязывается доктор - приемный час:
+  # для всех дочерних событий выставляем доктора как принимаюшего доктора.
   def add_attending_doctor_to_child
     if self.event.is_a? AppointmentHourEvent
       self.event.appointment_events.each do |e|
