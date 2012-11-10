cbdd7e0ba0867cde853a8d3ef29de139aef0ded8
nikita <nikita.kem@gmail.com>
2012-11-10 14:53:43 +0700

    #70 fix child model initializer

--- config/initializers/subclasses.rb
+++ config/initializers/subclasses.rb
@@ -1,3 +1,5 @@
-Event.select(:type).map(&:type).uniq
-User.select(:type).map(&:type).uniq
-Parameter.select(:type).map(&:type).uniq
+[:event, :user, :parameter].each do |c|
+  if ActiveRecord::Base.connection.tables.include?(c.to_s.pluralize) and !defined?(::Rake)
+    c.to_s.classify.constantize.select(:type).map(&:type).uniq
+  end
+end
