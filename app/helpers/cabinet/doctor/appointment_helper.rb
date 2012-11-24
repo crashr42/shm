109ebffb697d9ad95e76eee5ce1a69d6e3a50bb3
Denis <denis.kem@gmail.com>
2012-11-24 20:05:17 +0700

    Added get_appointment_organizer

--- app/helpers/cabinet/doctor/appointment_helper.rb
+++ app/helpers/cabinet/doctor/appointment_helper.rb
@@ -1,6 +1,11 @@
 module Cabinet::Doctor::AppointmentHelper
+
   def get_patient_by_event (event)
     @patient = PatientAttendee.find_by_event_id(event.id)
-    @patient = @patient.present? ? @patient.user.get_FI : "Not found patient attendee"
+    @patient.present? ? @patient.user.get_FI : "Not found patient attendee"
+  end
+
+  def get_appointment_organizer(appointment)
+   appointment.user_id.present? ? User.find(@a.user_id).get_FI() : "Not found event organizer"
   end
 end
