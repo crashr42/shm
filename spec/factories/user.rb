7de5fed9bc2d022e036eeafcd56441ef8adf9e66
nikita <nikita.kem@gmail.com>
2012-12-01 17:40:45 +0700

    Closed #71 fix test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
-  %w(user patient admin manager doctor).each do |r|
-    factory "#{r}_user".to_sym do
+  %w(user patient_user admin_user manager_user doctor_user).each do |r|
+    factory r.to_sym do
       address 'test'
       email 'test@test.com'
       password '123456'
       first_name 'test'
       last_name 'test'
-      type "#{r.capitalize}User"
+      type r.classify
     end
   end
 end
