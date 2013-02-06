f98295a7d2084956fecdc17578cefaaf87a45ea0
nikita <nikita.kem@gmail.com>
2013-02-06 21:59:14 +0700

    #85 add fake data for parameters

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,55 +29,55 @@ Specialty.create :name => 'Оттоларинголог'
 Specialty.create :name => 'Окулист'
 
 Diagnosis.create({
-                     :class_code => 'I',
-                     :class_name => 'Некоторые инфекционные и паразитарные болезни',
-                     :block_code => 'A30-A49',
-                     :block_name => 'Другие бактериальные болезни',
-                     :code       => 'A39.5',
-                     :code_name  => 'Менингококковая болезнь сердца'
-                 })
+    :class_code => 'I',
+    :class_name => 'Некоторые инфекционные и паразитарные болезни',
+    :block_code => 'A30-A49',
+    :block_name => 'Другие бактериальные болезни',
+    :code       => 'A39.5',
+    :code_name  => 'Менингококковая болезнь сердца'
+})
 
 Role.all.each do |role|
-  user = "#{role.name.capitalize!}User".constantize.new
+  user            = "#{role.name.capitalize!}User".constantize.new
   user.first_name = Faker::Name.first_name
-  user.last_name = Faker::Name.last_name
-  user.address = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
+  user.last_name  = Faker::Name.last_name
+  user.address    = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
   if user.is_a? PatientUser
-    user.policy = Faker::Lorem.characters 32
+    user.policy      = Faker::Lorem.characters 32
     user.doctor_user = DoctorUser.first
   end
   if user.is_a? DoctorUser
     user.specialty = Specialty.first(:offset => rand(Specialty.count))
   end
-  user.email = "#{role.name.downcase}@shm.com"
+  user.email    = "#{role.name.downcase}@shm.com"
   user.password = 123456
   user.save!
   UsersToRoles.create({:user => user, :role => role})
 end
 
 (0..10).each do
-  u = DoctorUser.new
+  u            = DoctorUser.new
   u.first_name = Faker::Name.first_name
-  u.last_name = Faker::Name.last_name
-  u.address = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
-  u.policy = Faker::Lorem.characters 32
-  u.email = Faker::Internet.email
-  u.password = u.email
-  u.specialty = Specialty.first(:offset => rand(Specialty.count))
+  u.last_name  = Faker::Name.last_name
+  u.address    = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
+  u.policy     = Faker::Lorem.characters 32
+  u.email      = Faker::Internet.email
+  u.password   = u.email
+  u.specialty  = Specialty.first(:offset => rand(Specialty.count))
   u.save!
   UsersToRoles.create({:user => u, :role => Role.find_by_name('doctor')})
 end
 
 DoctorUser.all.each do |d|
   from_date = DateTime.now.to_date - Random.rand(1..10).days
-  to_date = DateTime.now.to_date + Random.rand(1..10).days
+  to_date   = DateTime.now.to_date + Random.rand(1..10).days
   from_date.step(to_date).each do |date|
-    e = AppointmentHourEvent.new
-    e.summary = "Summary for appointment hour for user #{d.email}"
+    e             = AppointmentHourEvent.new
+    e.summary     = "Summary for appointment hour for user #{d.email}"
     e.description = "Description for appointment hour for user #{d.email}"
-    e.date_start = date.to_datetime.change({:hour => Random.rand(8..12)})
-    e.date_end = e.date_start.change({:hour => e.date_start.hour + Random.rand(1..6)})
-    e.user = d
+    e.date_start  = date.to_datetime.change({:hour => Random.rand(8..12)})
+    e.date_end    = e.date_start.change({:hour => e.date_start.hour + Random.rand(1..6)})
+    e.user        = d
     e.save!
 
     AttendingDoctorAttendee.create({:user => d, :event => e})
@@ -90,34 +90,34 @@ RuleParameterInput.create({:rule => 'утром'})
 RuleParameterInput.create({:rule => 'вечером'})
 
 BoolParameter.create({
-                         :name => 'Головная боль',
-                         :rule_parameter_input => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))
-                     })
+    :name                 => 'Головная боль',
+    :rule_parameter_input => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))
+})
 IntegerParameter.create({:name => 'Температура', :metadata => {
-    :default => '36',
+    :default    => '36',
     :validators => {
         :min => '0',
         :max => '100'
     }
-}, :rule_parameter_input => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
+}, :rule_parameter_input       => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
 SelectParameter.create({:name => 'Возрастная категория', :metadata => {
     :default => '0-9',
-    :values => %w(0-9 9-18 18-27 27-36)
-}, :rule_parameter_input => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
+    :values  => %w(0-9 9-18 18-27 27-36)
+}, :rule_parameter_input      => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
 StringParameter.create({:name => '2х2', :metadata => {
     :default => '4'
-}, :rule_parameter_input => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
+}, :rule_parameter_input      => RuleParameterInput.first(:offset => rand(RuleParameterInput.count))})
 
 (0..50).each do
-  u = PatientUser.new
-  u.first_name = Faker::Name.first_name
-  u.last_name = Faker::Name.last_name
-  u.address = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
-  u.policy = Faker::Lorem.characters 32
-  u.email = Faker::Internet.email
-  u.password = u.email
+  u             = PatientUser.new
+  u.first_name  = Faker::Name.first_name
+  u.last_name   = Faker::Name.last_name
+  u.address     = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
+  u.policy      = Faker::Lorem.characters 32
+  u.email       = Faker::Internet.email
+  u.password    = u.email
   u.doctor_user = DoctorUser.first(:offset => rand(DoctorUser.count))
-  u.diagnosis = Diagnosis.first(:offset => rand(Diagnosis.count))
+  u.diagnosis   = Diagnosis.first(:offset => rand(Diagnosis.count))
   u.save!
   UsersToRoles.create({:user => u, :role => Role.find_by_name('patient')})
 
@@ -136,20 +136,59 @@ StringParameter.create({:name => '2х2', :metadata => {
 end
 
 (0..10).each do
-  b = Bid.new
-  b.address = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
-  b.email = Faker::Internet.email
-  b.first_name = Faker::Name.first_name
-  b.last_name = Faker::Name.last_name
-  b.third_name = Faker::Name.last_name
+  b               = Bid.new
+  b.address       = "#{Faker::Address.country} #{Faker::Address.city} #{Faker::Address.street_address}"
+  b.email         = Faker::Internet.email
+  b.first_name    = Faker::Name.first_name
+  b.last_name     = Faker::Name.last_name
+  b.third_name    = Faker::Name.last_name
   b.passport_scan = open("#{Rails.root}/spec/images/test.jpg")
-  b.policy = Faker::Lorem.characters(5)
+  b.policy        = Faker::Lorem.characters(5)
   b.save!
 end
 
 #Для каждого доктора заполним специальность
-DoctorUser.all.each {|doctor|
+DoctorUser.all.each { |doctor|
   doctor.specialty_id = Specialty.first(order: 'RANDOM()').id
-  doctor.save
+  doctor.save!
 }
 
+PatientUser.all.each do |p|
+  (0..50).each do
+    pd = ParametersData.new({
+        :parameter_id => BoolParameter.first.id,
+        :user_id      => p.id,
+        :value        => %w(true false).sample
+    })
+    pd.created_at = DateTime.now.to_date - rand(1..10).days + rand(1..12).hours
+    pd.save!
+  end
+  (0..50).each do
+    pd = ParametersData.new({
+        :parameter_id => IntegerParameter.first.id,
+        :user_id      => p.id,
+        :value        => rand(100)
+    })
+    pd.created_at = DateTime.now.to_date - rand(1..10).days + rand(1..12).hours
+    pd.save!
+  end
+  (0..50).each do
+    parameter = SelectParameter.first
+    pd = ParametersData.new({
+        :parameter_id => parameter.id,
+        :user_id      => p.id,
+        :value        => parameter.metadata[:values].sample
+    })
+    pd.created_at = DateTime.now.to_date - rand(1..10).days + rand(1..12).hours
+    pd.save!
+  end
+  (0..50).each do
+    pd = ParametersData.new({
+        :parameter_id => StringParameter.first.id,
+        :user_id      => p.id,
+        :value        => Faker::Lorem.characters(20)
+    })
+    pd.created_at = DateTime.now.to_date - rand(1..10).days + rand(1..12).hours
+    pd.save!
+  end
+end
