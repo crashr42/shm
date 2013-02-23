a771568eb9d8d4a825f07166aaa2209792a09396
nikita <nikita.kem@gmail.com>
2013-02-23 22:09:23 +0700

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def link_to_active(*args, &block)
-    name         = args[0]
-    options      = args[1] || {}
+    name = args[0]
+    options = args[1] || {}
     html_options = args[2]
     if current_page?(options)
       if html_options.present? && html_options.include?(:class)
@@ -14,9 +14,9 @@ module ApplicationHelper
   end
 
   def link_to_active_wrapper(*args, &block)
-    name         = args[0]
-    wrapper      = args[1]
-    options      = args[2] || {}
+    name = args[0]
+    wrapper = args[1]
+    options = args[2] || {}
     html_options = args[3]
     content_tag(wrapper, link_to(name, options, html_options, &block), current_page?(options) ? {:class => 'active'} : {})
   end
