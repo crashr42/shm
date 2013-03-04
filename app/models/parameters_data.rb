b3c537090655dcb6aa18c161d74110ebecc0e56c
nikita <nikita.kem@gmail.com>
2013-03-04 22:10:27 +0700

    publish message on user fill parameter value

--- app/models/parameters_data.rb
+++ app/models/parameters_data.rb
@@ -4,6 +4,8 @@ class ParametersData < ActiveRecord::Base
   # Параметр, чье значение ввел пациент
   belongs_to :parameter
 
+  after_commit :publish, :on => :create
+
   validates :value, :presence => true
   validates :user_id, :presence => true
   validates :parameter_id, :presence => true
@@ -12,9 +14,13 @@ class ParametersData < ActiveRecord::Base
   attr_accessible :parameter, :parameter_id, :patient_user, :user_id, :value
 
   private
-
   # Валидация введенного значения параметра
   def valid_for_parameter_metadata
     errors.add(:value, 'parameter.data.invalid.value') unless self.parameter.validate_value(self.value)
+  end
+
+  def publish
+    # Сообщаем что пациент заполнил параметр
+    patient_user.push({:user => patient_user, :parameter => parameter, :data => self}, 'parameter_data.create')
   end
 end
